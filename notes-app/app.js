509e9e32a27f187d793629f6f00f2df46f1fbee5
Jay Han <jungho1109@gmail.com>
2020-07-11 16:30:44 +0900

    npm tutorial

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,9 @@
 const getNotes = require("./notes");
+const chalk = require("chalk");
 
 const msg = getNotes();
 
 console.log(msg);
+
+console.log(chalk.green.bold("Success!"));
+console.log(chalk.red("Error!"));
