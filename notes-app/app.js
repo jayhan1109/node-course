5480ad4ad448c3db76c974b1d5f539c8c7f68a6e
Jay Han <jungho1109@gmail.com>
2020-07-11 20:29:49 +0900

    Yargs command

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,42 @@
 const getNotes = require("./notes");
 const chalk = require("chalk");
+const yargs = require("yargs");
 
-const msg = getNotes();
+// Customize yargs version
+yargs.version("1.1.0");
 
-console.log(msg);
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note!");
+  },
+});
 
-console.log(chalk.green.bold("Success!"));
-console.log(chalk.red("Error!"));
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing the note!");
+  },
+});
+
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "List your notes",
+  handler: function () {
+    console.log("Listing out all notes!");
+  },
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("Reading a note!");
+  },
+});
